c7852e3b7e493b4d83df630942308c11154ddaeb
Sebastian Gingter <sebastian@gingter.org>
2018-03-07 21:42:12 +0100

    Initial app

---
+++ Canonn-Api/EddnEventBridgeSample/Application.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace EddnEventBridgeSample
+{
+	public class Application
+	{
+		private readonly ILogger _logger;
+
+		public Application(ILogger<Application> logger)
+		{
+			_logger = logger;
+		}
+
+		public async Task Run(CancellationToken token)
+		{
+			_logger?.LogInformation("Application is starting");
+
+			while (true)
+			{
+				if (!token.IsCancellationRequested)
+				{
+					await Task.Delay(TimeSpan.FromSeconds(2));
+					_logger?.LogInformation("Application is running...");
+				}
+
+				if (token.IsCancellationRequested)
+				{
+					break;
+				}
+			}
+
+			_logger?.LogInformation("Application completed");
+		}
+	}
+}
---
+++ Canonn-Api/EddnEventBridgeSample/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace EddnEventBridgeSample
+{
+	class Program
+	{
+		static async Task<int> Main(string[] args)
+		{
+			InitializeLogging();
+
+			try
+			{
+				// Actual application code
+				var sp = BuildServiceProvider();
+
+				using (var cts = new CancellationTokenSource())
+				{
+					Console.CancelKeyPress += (s, e) =>
+					{
+						Log.Logger.Information("Ctrl-C received. Trying to shutdown gracefully.");
+						cts.Cancel(false);
+					};
+
+					var app = sp.GetRequiredService<Application>();
+					await app.Run(cts.Token);
+				}
+			}
+			catch (Exception ex)
+			{
+				Log.Logger.Fatal(ex, "Fatal error on application execution.");
+				return 1;
+			}
+			finally
+			{
+				Log.CloseAndFlush();
+			}
+
+#if DEBUG
+			if (System.Diagnostics.Debugger.IsAttached)
+			{
+				Console.ReadKey();
+			}
+#endif
+
+			return 0;
+		}
+
+		private static void InitializeLogging()
+		{
+			Log.Logger = new LoggerConfiguration()
+				.MinimumLevel.Information()
+				.Enrich.FromLogContext()
+				.Enrich.WithProperty("Application", "EddnEventBridgeSample")
+				.Enrich.WithProcessId()
+				.Enrich.WithProcessName()
+				.Enrich.WithThreadId()
+				.Enrich.WithMachineName()
+				.Enrich.WithMemoryUsage()
+				.Enrich.WithUserName()
+				.WriteTo.Console()
+				.WriteTo.Seq("http://localhost:5341")
+				.CreateLogger();
+		}
+
+		private static IServiceProvider BuildServiceProvider()
+		{
+			var services = new ServiceCollection()
+				.AddLogging(lb => lb.AddSerilog())
+				.AddTransient<Application>();
+
+			return services.BuildServiceProvider();
+		}
+	}
+}
